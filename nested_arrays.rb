2d209876a4aed6b8200590bf27e0b26d4cd8b7be
Soon Seng <soonseng.ee@thetechyhub.com>
2016-09-08 10:12:50 +0800

    Manually created array

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -1,4 +1,38 @@
 # Objective 2: Chessboard
+chessboard = [	["B Rook", "B Knight", "B Bishop", "B Queen", "B King", "B Bishop", "B Knight", "B Rook"],
+				["B Pawn", "B Pawn", "B Pawn", "B Pawn", "B Pawn", "B Pawn", "B Pawn", "B Pawn"],
+				["", "", "", "", "", "", "", ""],
+				["", "", "", "", "", "", "", ""],
+				["", "", "", "", "", "", "", ""],
+				["", "", "", "", "", "", "", ""],	
+				["W Pawn", "W Pawn", "W Pawn", "W Pawn", "W Pawn", "W Pawn", "W Pawn", "W Pawn"],
+				["W Rook", "W Knight", "W Bishop", "W Queen", "W King", "W Bishop", "W Knight", "W Rook"] ]
 
 
 # Objective 3: Data table
+data_table = [ 	["Number", "Name", "Position", "Points per game"],
+				[ 12, "Joe Schmo", "Center", [14, 32 ,7, 0, 23]],
+				[ 9, "Ms. Buckets", "Point Guard", [19, 0, 11, 22, 0]],
+				[ 31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+				[ 18, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
+				[ 22, "MK DiBoux", "Small Forward", [11, 0, 23, 17, 0]] ]
+
+
+
+
+def create_board()
+	board = []
+	row = 8 #boxes downwards
+ 	columns = 8  #boxes horizontal
+
+ 	for i in 0..row
+ 		#puts "hello"
+ 	end
+#do later
+end	
+
+create_board()			
+
+#puts chessboard[7][0] == "W Rook"
+#puts data_table[3][2] == "Shooting Guard"
+#puts data_table [1][3] == [14, 32, 7, 0, 23] 
